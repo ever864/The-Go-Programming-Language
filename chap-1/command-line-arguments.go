6db707dc4fa645d679e380cc1f2f46c1228b3ade
Ever Francisco Ponce Lara <ever864@gmail.com>
2025-01-28 23:28:04 -0500

    feat: add another form to make for

--- chap-1/command-line-arguments.go
+++ chap-1/command-line-arguments.go
@@ -6,10 +6,18 @@ import (
 )
 
 func main() {
-	var s, sep string
-	for i := 1; i < len(os.Args); i++ {
-		s += sep + os.Args[i]
-		sep = " "
+	//var s, sep string
+	//for i := 1; i < len(os.Args); i++ {
+	//	s += sep + os.Args[i]
+	//	sep = " "
+	//}
+	//
+
+	s, sep := "", ""
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = " ever "
 	}
+
 	fmt.Println(s)
 }
